2033b1dee2fa48c0b9a559c90a7cdfa3f621c86f
KonulBayramova <konul.bairamovaa@gmail.com>
2025-02-08 18:21:10 +0400

    Custom user model was modified
    Fullname was changed to first name and last name and they are required fields for registration

--- src/account/models/custom_user.py
+++ src/account/models/custom_user.py
@@ -11,22 +11,24 @@ class CustomUserManager(BaseUserManager):
     #Custom user model manager where email is the unique identifiers
     #for authentication instead of usernames.
     
-    def create_user(self, email, fullname, password, **extra_fields):
+    def create_user(self, email, first_name, last_name, password, **extra_fields):
         
         # Create and save a user with the given email and password.
        
         if not email:
-            raise ValueError(_("The Email should be entered"))
-        if not fullname:
-            raise ValueError(_("The Fullname should be entered"))
+            raise ValueError("The Email should be entered")
+        if not first_name:
+            raise ValueError("The First name should be entered")
+        if not last_name:
+            raise ValueError("The Last name should be entered")
         
         email = self.normalize_email(email)
-        user = self.model(email=email, fullname=fullname, **extra_fields)
+        user = self.model(email=email, first_name=first_name, last_name=last_name, **extra_fields)
         user.set_password(password)
         user.save()
         return user
 
-    def create_superuser(self, email, fullname, password, **extra_fields):
+    def create_superuser(self, email, first_name, last_name, password, **extra_fields):
    
         # Create and save a SuperUser with the given email and password.
         extra_fields.setdefault("is_staff", True)
@@ -37,17 +39,18 @@ class CustomUserManager(BaseUserManager):
             raise ValueError(_("Superuser must have is_staff=True."))
         if extra_fields.get("is_superuser") is not True:
             raise ValueError(_("Superuser must have is_superuser=True."))
-        return self.create_user(email, fullname, password, **extra_fields)
+        return self.create_user(email, first_name, last_name, password, **extra_fields)
 
 class CustomUserModel(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(db_index=True, unique=True)
-    fullname = models.CharField(max_length=100)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=150, null=True)
     is_staff = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
     date_joined = models.DateTimeField(default=timezone.now)
 
     USERNAME_FIELD = "email"
-    REQUIRED_FIELDS = ["fullname"]
+    REQUIRED_FIELDS = ["first_name", "last_name"]
 
     objects = CustomUserManager()
 
@@ -57,4 +60,4 @@ class CustomUserModel(AbstractBaseUser, PermissionsMixin):
         verbose_name_plural = 'Users'
 
     def __str__(self):
-        return "{} -> {}".format(self.email, self.fullname)
+        return "{} -> {} -> {}".format(self.email, self.first_name, self.last_name)
